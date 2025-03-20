c1d4af7bdfd9c3a61a7563e08546ad469bd32e68
KimJin0315 <xnxl159@naver.com>
2025-03-21 00:56:40 +0900

    home

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,42 +1,52 @@
 ﻿#include <iostream>
 
-#define SIZE 5
-
 using namespace std;
+
+int Find(int list[], int left, int right)
+{
+    if (left == right)
+    {
+        return list[left];
+    }
+    else
+    {
+        int middle = (left + right) / 2;
+
+        int leftMax = Find(list, left, middle);
+        int rightMax = Find(list, middle + 1, right);
+
+        if (leftMax < rightMax)
+        {
+            return rightMax;
+        }
+        else
+        {
+            return leftMax;
+        }
+    }
+}
 
 int main()
 {
-#pragma region 삽입 정렬
-    // 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
-    // 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다/
+#pragma region 분할 정복
+    // 주어진 2개 이상의 부분으로 문제를 나눈 뒤 각 부분
+    // 문제에 대한 답을 재귀 호출을 이용하여 계산한 다음, 그
+    // 답으로 부터 전체 문제의 답을 계산해내는 알고리즘입니다.
 
-    int list[SIZE] = { 9,6,8,1,3 };
+    // 분할 (Divide) : 주어진 문제를 두 개 혹은 그 이상의 형식으로 나눈다.
 
-    for (int i = 0; i < SIZE - 1; i++)
-    {
-        int key = list[i + 1];
+    // 정복 (Conquer) : 나누어진 문제를 재귀적으로 해결해서 나누어진 문제를
+    // 더 이상 나누어서 문제가 필요없을 때까지 계속 분할합니다.
 
-        for (int j = 0; j < SIZE - 1; j++)
-        {
-            if (key < list[j])
-            {
-                list[j + 1] = list[j];
-                list[j] = key;
-            }
-            else if (key > list[j])
-            {
-                
-            }
-        }
-    }
+    // 통합 (Combine) : 나누어서 해결한 문제들을 통합해서 원래 문제의 해답을 생성합니다.
 
-    for (int i = 0; i < SIZE; i++)
-    {
-        cout << list[i] << " ";
-    }
+    int list[] = { 20,15,99,1 };
+
+    int size = sizeof(list) / sizeof(int);
+
+    cout << Find(list, 0, size - 1);
 
 #pragma endregion
-
 
     return 0;
 }
